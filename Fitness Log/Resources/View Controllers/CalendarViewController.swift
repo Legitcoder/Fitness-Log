553f2fd2035ca8a3330b7f4120095d8872dc7ec9
Moin Uddin <U_Moin75@yahoo.com>
2018-12-12 00:43:21 -0500

    Get Calendar to show up on screen

--- Fitness Log/Resources/View Controllers/CalendarViewController.swift
+++ Fitness Log/Resources/View Controllers/CalendarViewController.swift
@@ -7,16 +7,18 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
 class CalendarViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+     calendarView.ibCalendarDelegate = self
+     calendarView.calendarDataSource = self
     }
     
-
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
+    
     /*
     // MARK: - Navigation
 
@@ -26,5 +28,46 @@ class CalendarViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    let formatter = DateFormatter()
+}
 
+
+extension CalendarViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        let myCustomCell = cell as! CustomCell
+        sharedFunctionToConfigureCell(myCustomCell: myCustomCell, cellState: cellState, date: date)
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        cell.dateLabel.text = cellState.text
+        return cell
+    }
+    
+    func sharedFunctionToConfigureCell(myCustomCell: CustomCell, cellState: CellState, date: Date) {
+        myCustomCell.dateLabel.text = cellState.text
+//        if testCalendar.isDateInToday(date) {
+//            myCustomCell.backgroundColor = red
+//        } else {
+//            myCustomCell.backgroundColor = white
+//        }
+        // more code configurations
+        // ...
+        // ...
+        // ...
+    }
+    
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        let startDate = formatter.date(from: "2018 01 01")!
+        let endDate = formatter.date(from: "2018 12 31")!
+        
+        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
+        return parameters
+    }
+    
+    
 }
---
+++ Fitness Log/Resources/View Controllers/CustomCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCell.swift
+//  Fitness Log
+//
+//  Created by Moin Uddin on 12/11/18.
+//  Copyright © 2018 Moin Uddin. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+    @IBOutlet weak var dateLabel: UILabel!
+}
